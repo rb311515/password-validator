51ede7b9a6f8d576cf89288d8d78ef178161ec5c
thomasrs <rb311515@dal.ca>
2017-05-04 12:57:34 -0300

    Added button and text field to finish Assignment 1

--- app/src/main/java/com/acme/csci3130/MainActivity.java
+++ app/src/main/java/com/acme/csci3130/MainActivity.java
@@ -2,12 +2,30 @@ package com.acme.csci3130;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+
+    String text="Hello World!";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    protected void ButtonPress(View view) {
+        //value in the text field
+        EditText enter = (EditText)findViewById(R.id.editText);
+
+        TextView display = (TextView)enter;
+
+        ((TextView)findViewById(R.id.helloText)).setText(display.getText());
+
+
+
+    }
+
 }
